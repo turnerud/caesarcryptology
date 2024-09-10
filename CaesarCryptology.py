459f57ce5a1de4b85797a0371140ba2d48e1fe62
turnerud <143515347+turnerud@users.noreply.github.com>
2024-09-09 23:41:42 -0500

    Add files via upload

---
+++ CaesarCryptology.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 09:23:55 2024
+
+@author: simon
+
+EncodeCaesar done 
+DecodeCaesarShift done 
+DecodeCaesar done 
+
+
+"""
+
+global alphabet;
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+global upperAlphabet;
+upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+def EncodeCaesar():
+    originalText = input("What is your plaintext? ");
+    shift = int(input("What is your shift? "))
+    
+    list_of_chars = list(originalText);
+    
+    cipherTextChars = [];
+    
+    for x in list_of_chars:
+        
+        if x in alphabet:
+            
+            index_of_x = alphabet.index(x);
+            
+            cipherTextChars.append(alphabet[(index_of_x + shift) % 26]);
+        
+        elif x in upperAlphabet:
+            
+            index_of_x = upperAlphabet.index(x);
+            
+            cipherTextChars.append(upperAlphabet[(index_of_x + shift) % 26]);
+            
+        else:
+            
+            cipherTextChars.append(x);
+            
+    cipherText = ''.join([str(item) for item in cipherTextChars])
+    print(cipherText)
+
+
+
+
+def DecodeCaesarShift():
+    
+    global shift;
+    cipherText = input("What is your cipher text? ")
+    shift = int(input("What is your shift? "))
+
+    
+    global list_of_cipher_chars;
+    
+    list_of_cipher_chars = list(cipherText);
+    originalTextChars = [];
+    for x in list_of_cipher_chars:
+        
+        if x in alphabet:
+            
+            index_of_x = alphabet.index(x);
+            originalTextChars.append(alphabet[(index_of_x - shift) % 26]);
+            
+        elif x in upperAlphabet:
+            
+            index_of_x = upperAlphabet.index(x);
+            originalTextChars.append(upperAlphabet[(index_of_x - shift) % 26]);
+        
+        else:
+            
+            originalTextChars.append(x);
+            
+    originalText = ''.join([str(item) for item in originalTextChars])
+    print(originalText)
+    return list_of_cipher_chars;
+    return shift;
+    
+    
+def DecodeCaesar():
+    
+    plainText = input("What is your plain text? ");
+    DecodeCaesarShift();
+    elementNumber = 0;
+    shiftNumber = 0;
+    
+    list_of_original_chars = list(plainText);
+    
+    for x in list_of_cipher_chars:
+        
+        match = False;
+        
+        if x in alphabet:
+            
+            index_of_cipher_char = alphabet.index(x);
+            
+            while match == False:
+                
+                if alphabet[(index_of_cipher_char - shiftNumber) % 26] == list_of_original_chars[elementNumber]:
+                
+                    elementNumber += 1;
+                    match = True;
+                
+                else:
+                
+                    shiftNumber += 1;
+                    
+            
+        elif x in upperAlphabet:
+            
+            index_of_cipher_char = upperAlphabet.index(x);
+            
+            while match == False:
+                
+                if upperAlphabet[(index_of_cipher_char - shiftNumber) % 26] == list_of_original_chars[elementNumber]:                
+                
+                    elementNumber += 1;
+                    match = True;
+                
+                else:
+                
+                    shiftNumber += 1;
+                    
+        else:
+            
+             elementNumber += 1;
+
+        if match == False:
+            
+             shiftNumber += 1;
+             elementNumber += 1;
+
+    print("Your probable shift is ", shiftNumber);
+    
+  
+
+EncodeCaesar();
+DecodeCaesarShift();
+DecodeCaesar();
+
+
+
+    
+    
